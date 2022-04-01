41fb53d2e9d6252f1c36748420d01629a5bfda44
abrunhuemer <49669832+abrunhuemer@users.noreply.github.com>
2022-04-01 12:43:54 +0200

    Init backend (#9)
    * add initial Java Spring Boot backend with GraphQL interface
    
    * change graphql api endpoint

---
+++ backend/src/test/java/com/brunhuemer/tennisticker/TennisTickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunhuemer.tennisticker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisTickerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
